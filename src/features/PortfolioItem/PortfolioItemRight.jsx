71e6f236b43550363876f2434dcea06af61a079f
Tushar Karamchandani <tusharkaramchandani25@gmail.com>
2023-09-19 00:02:00 +0530

    changed font

--- src/features/PortfolioItem/PortfolioItemRight.jsx
+++ src/features/PortfolioItem/PortfolioItemRight.jsx
@@ -5,7 +5,7 @@ const PortfolioItemRight = ({ data }) => {
     <div className="px-6 md:px-0 container mx-auto max-w-2xl">
       <h1
         style={{ viewTransitionName: `projectTitle-${data?.id}` }}
-        className="text-white text-7xl font-black tracking-wide py-4 m-0"
+        className="text-white text-5xl md:text-7xl font-black tracking-wide py-4 m-0"
       >
         {data?.projectName}
       </h1>
