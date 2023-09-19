4efce6690eb52123af0d9ffef60d554e7c10ab77
Tushar Karamchandani <tusharkaramchandani25@gmail.com>
2023-09-19 22:54:20 +0530

    deployed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { LoaderLayout } from "@/components";
 import "./globals.css";
 import { Titillium_Web } from "next/font/google";
+import Script from "next/script";
 
 const titilliumWeb = Titillium_Web({
   subsets: ["latin"],
@@ -15,6 +16,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-BS6L24S39F`}
+      />
+
+      <Script id="googleTag" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BS6L24S39F', {
+            page_path: window.location.pathname,
+            });
+          `}
+      </Script>
       <body className={titilliumWeb.className}>
         <LoaderLayout>{children}</LoaderLayout>
       </body>
